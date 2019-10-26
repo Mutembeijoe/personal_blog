8ab562c18f89bae9b23715676b9a77cfe96f9fd9
mutembeijoe <mutembeijoe@gmail.com>
2019-10-26 22:33:37 +0300

    adds register.html with a register form and adds a main.css file

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template
-
+from app.users.forms import RegistrationForm
 
 users = Blueprint('users', __name__)
 
 
 @users.route('/register', methods = ['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegistrationForm()
+    return render_template('register.html', form = form )
