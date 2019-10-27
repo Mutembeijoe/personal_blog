32ebf50e798c0bbd1d41a3e3c95ff0a0ed4d4ace
mutembeijoe <mutembeijoe@gmail.com>
2019-10-27 14:31:09 +0300

    intergrates flask-migrate to track db migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from app.config import config_options
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
@@ -19,6 +21,7 @@ def create_app(config_name):
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    migrate.init_app(app,db)
 
     from app.main.routes import main
     from app.users.routes import users
