9d4030b1f3d7d80ff3a0a00284c02687456a7b88
mutembeijoe <mutembeijoe@gmail.com>
2019-10-27 20:04:24 +0300

    adds bootstrap card to display posts at the home page and adds categories to the navbar

--- app/main/routes.py
+++ app/main/routes.py
@@ -6,7 +6,10 @@ main = Blueprint('main',__name__)
 
 @main.route('/')
 @main.route('/home')
-def home():
-    posts = Post.query.all()
-    # avatar = url_for('static', filename=f'profile_pics/{current_user.image_file}')
+@main.route('/home/<string:category>')
+def home(category = None):
+    if category:
+        posts = Post.query.filter_by(category=category)
+    else:
+        posts = Post.query.all()
     return render_template('home.html', posts=posts, title = 'Home')
