265901f643d558127a4f315cb69cd7d046c02291
mutembeijoe <mutembeijoe@gmail.com>
2019-10-27 16:18:44 +0300

    intergrates the logic for post update

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash,redirect,url_for,render_template
+from flask import Blueprint, flash,redirect,url_for,render_template,abort,request
 from app.posts.forms import PostForm
 from flask_login import login_required, current_user
 from app.models import Post
@@ -17,4 +17,29 @@ def new_post():
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('main.home'))
-    return render_template('create_post.html',form = form)
+    return render_template('create_post.html',form = form, legend = "Create Post")
+
+@posts.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@posts.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('posts.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post',
+                           form=form, legend='Update Post')
+
